ec2b933ef55302b0bd886e4dfd581faa2c7e15c3
ahansen95 <98108947+ahansen95@users.noreply.github.com>
2022-10-25 01:47:03 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,21 +4,21 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0;
  if array.length == 0
    return 0
  elseif arr.length >=1
-   return arr.inject(0){ |sum, x| sum + x }
+   return array.each { |a| sum+=a }
 
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
- return sum(arr.sort.last(2))
+
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
- return !!arr.uniq.combination(2).detect{|a,b| a + b == n}
 end
 
 # Part 2
