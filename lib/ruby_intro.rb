1363d27b12c48018c3fc4aa1d00a9303659fac94
Austin Hansen <ahansen2@uccs.edu>
2022-10-27 01:29:04 +0000

    all parts completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -22,7 +22,7 @@ end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
-
+  !!arr.uniq.combination(2).detect{|x,y| x + y == n}
 end
 
 # Part 2
@@ -34,14 +34,30 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  /^[^aeiouAEIOU\d\W]/i =~ s ? true : false
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  return false if s.empty? || s =~ /[^01]/
+  (s == '0') || s.end_with?('00')
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-end
+  # YOUR CODE HERE
+    attr_accessor :isbn, :price
+    
+    def initialize isbn, price
+      raise ArgumentError.new("ISBN is empty") unless isbn.length > 0
+      raise ArgumentError.new("Price should be larger than 0") unless price > 0
+      @isbn = isbn
+      @price = Float(price)
+    end
+    
+    def price_as_string
+      format("$%.2f", @price)
+    end
+  
+  end
